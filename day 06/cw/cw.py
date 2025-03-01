0e9e0cf196944c5f65d5c19fcbbc6e206a893111
nikolozijavaxishvili <nikolozijavaxa305@gmail.com>
2025-03-01 22:56:48 +0400

    second commit

---
+++ day 06/cw/cw.py
@@ -0,0 +1,29 @@
+#1) შექმენით mybalance-ის ცვლადი, შემდეგ გაზარდეთ ამ ცვლადის მნიშვნელობა 2-თ უბრალო ფორმით, შემდეგ ისევ გაზარდეთ მნიშვნელობა 3-ით ოღონდ გამოიყენეთ assigment operator, ამის შემდეგ თქვენი სურვილისამებრ შეცვალეთ ცვლადის მნიშვნელობა იგივე assingment operator-ის საშვალებით, კოდი ახსენით კომენატრებით, ჩამოწერეთ ყველა საუკეტესო პრაქტიკა და მიუწერეთ განმარტებები
+
+user = "nikolas"
+mybalance = 190
+print ("opens diamond chest ")
+#aq unda gamoviyenot asingment  opperator
+mybalance+=56
+print ("balance gaizarda 56 ")
+mybalance=246
+#gavzarde my balance cvladi
+print("mtliani balance aris 246")
+
+
+
+
+#task2
+
+#2) გააკეთეთ პროგრამა რომელშიც გექნებათ ცვლდი სადაც შეინახავთ თქვენს hobby-ის, ასევე შექმენით string ტიპის ცვლადი რომელშიც შეინახვათ თქვენთვის სასურველ მესიჯს, შემდეგ ეს ორი ტექსტი დაბეჭდეთ ერთად, ასევე აიღეთ 2 integer ტიპის ცვლადი და დაბეჭდეთ მათი შეკრებაც, თქვენი სიტყვებით ახსენით რა განსხვავებაა ამ ორი სხვადასხვა ტიპის ცვლადების დამატების ოპერაციებს შორის, რა ეწოდება თითოეულ მოვლენას
+
+myhobbu = "programing"
+mymesiji = "mymesiji"
+print(myhobbu + mymesiji)
+#aq vamatebt sityvebs amas qvia stringebis mimateba 
+num = 213
+num1 = 23
+print(num)
+print(num1)
+print(num + num1)
+#es aris integerebis mimateba aq ubralod ricxvebi vamatebt
