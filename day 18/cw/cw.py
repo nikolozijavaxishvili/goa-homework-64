25232e76671d907fb01d638b441abb60da82cca7
nikolozijavaxishvili <nikolozijavaxa305@gmail.com>
2025-04-12 19:52:33 +0400

    second commit

---
+++ day 18/cw/cw.py
@@ -0,0 +1,15 @@
+num = 101
+
+while num <= 300:
+   print(num)
+   num += 2
+
+
+
+
+
+
+
+grade= int( input("enter your grade"))
+if grade > 50:
+   print("chaaaabaaaaaaaaaaaaaaareeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
